e252a750d52813979719c0ebcdb8c81497cfa0f3
novoronetskaya <novoronetskaya@edu.hse.ru>
2022-03-24 14:44:35 +0300

    Add user

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    last_name = models.CharField
+    first_name = models.CharField
+    birth_year = models.IntegerField
+    city = models.CharField
+    university = models.CharField
+    vacancy = models.CharField
+    experience = models.CharField
+    email = models.EmailField
+
+    USERNAME_FIELD = email
