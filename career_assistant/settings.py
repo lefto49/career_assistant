0e39159366245af58f435a9be79612700aeb7369
lefto49 <78983047+lefto49@users.noreply.github.com>
2022-05-01 13:11:48 +0300

    Fix bugs

--- career_assistant/settings.py
+++ career_assistant/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 import rest_framework_simplejwt
 
@@ -147,4 +148,8 @@ REST_FRAMEWORK = {
   'DEFAULT_AUTHENTICATION_CLASSES': (
     'rest_framework_simplejwt.authentication.JWTAuthentication',
   ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
